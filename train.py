b922c6325a2209d83d92d0ee9be4ec549bab4165
chrisliu298 <yuhaoliu298@gmail.com>
2022-08-30 11:31:10 -0700

    Add aug arg

--- train.py
+++ train.py
@@ -34,6 +34,7 @@ def parse_args():
     parser.add_argument(
         "--dataset", type=str, default="cifar10", choices=DATASETS.keys()
     )
+    parser.add_argument("--aug", action="store_true")
     # training
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--max_epochs", type=int, default=1000)
